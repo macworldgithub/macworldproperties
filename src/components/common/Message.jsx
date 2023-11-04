2a3aea1c50f24ef734812f5da5864c7ba55c6d1d
Asad Ullah Khan <asadkhan2168735@gmail.com>
2023-11-04 23:22:19 +0500

    WIP: Advanced Page

--- src/components/common/Message.jsx
+++ src/components/common/Message.jsx
@@ -41,7 +41,7 @@ const Message = ({ listingOwner, contactPerson, email, phone }) => {
               <div className="flex flex-col mt-4">
                 <div className="flex justify-between">
                   <h3 className="font-bold">Email</h3>
-                  <p>{`${email.substring(0,15)}..`}</p>
+                  <p>{email?.length > 15 ? `${email?.substring(0,15)}..`: email}</p>
                 </div>
                 <hr className="border-gray-500" />
               </div>
