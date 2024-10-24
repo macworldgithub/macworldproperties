535b7ddab4f70c51cbb73922edd30262765258f0
salman-aly <alysalman15@gmail.com>
2024-10-24 18:17:55 +0500

    ..

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -32,6 +32,7 @@ const Footer = () => {
       .then((response) => {
         toast.success(response.message);
        setEmail('');
+       
       })
       .catch((error) => {
         console.log("Error:", error.response.data);
